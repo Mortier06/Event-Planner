b3355e84e093507bf9f9646d8a7267124f43ac88
Mortier06 <185471447+Mortier06@users.noreply.github.com>
2025-12-29 22:25:41 +0100

    Migrations and db builded

---
+++ Backend/Persistence.Entities/EventDBContext.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Entities;
+
+public class EventDBContext : DbContext
+{
+    public DbSet<Locatie> Locaties { get; set; }
+    public DbSet<Evenement> Evenementen { get; set; }
+    public DbSet<Taak> Taken { get; set; }
+    private string connectionString = "Server=127.0.0.1;Port=8555;Database=EventPlanner;User=eventplanner;Password=eventplanner123;SslMode=Preferred;";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseMySql(
+             connectionString,
+             new MySqlServerVersion(ServerVersion.AutoDetect(connectionString))
+         );
+    }
+}
---
+++ Backend/Persistence.Entities/Migrations/20251229211956_InitialCreate.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Locaties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Naam = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Beschrijving = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    GpsLat = table.Column<int>(type: "int", nullable: false),
+                    GpsLon = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locaties", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Evenementen",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LocatieId = table.Column<int>(type: "int", nullable: false),
+                    Naam = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    StartDateTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    EindDateTime = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Evenementen", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Evenementen_Locaties_LocatieId",
+                        column: x => x.LocatieId,
+                        principalTable: "Locaties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Taken",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EvenementId = table.Column<int>(type: "int", nullable: false),
+                    Naam = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Beschrijving = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Belangrijkheid = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Status = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DeadLineDateTime = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Taken", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Taken_Evenementen_EvenementId",
+                        column: x => x.EvenementId,
+                        principalTable: "Evenementen",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evenementen_LocatieId",
+                table: "Evenementen",
+                column: "LocatieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Taken_EvenementId",
+                table: "Taken",
+                column: "EvenementId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Taken");
+
+            migrationBuilder.DropTable(
+                name: "Evenementen");
+
+            migrationBuilder.DropTable(
+                name: "Locaties");
+        }
+    }
+}
